0ae05877d5e2cb4f62d824188d05089fa9a2d595
iron-arya <71444583+iron-arya@users.noreply.github.com>
2020-10-07 05:05:38 -0700

    Add files via upload

--- bullet.js
+++ bullet.js
@@ -8,7 +8,7 @@ function setup() {
   createCanvas(1600,400);
   speed=random(55,90);
   weight=random(400,1500);
-  bullet= createSprite(50,200,50,50);
+  bullet = createSprite(50,200,50,50);
   bullet.velocityX=speed;
   wall = createSprite(1500,200,60, height/2);
 
@@ -29,7 +29,7 @@ function draw() {
    } 
 
    if(deformation<180 && deformation>180){
-    bullet.shapeColor=color(230,230,0);
+     bullet.shapeColor=color(230,230,0);
    }
    
    if(deformation<100){
