dc7fac3a6c87e681d840ab18504f5766fc8b8113
iron-arya <71444583+iron-arya@users.noreply.github.com>
2020-10-07 07:04:32 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,65 @@
-var car,wall;
-var speed,weight;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-
-
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
 function setup() {
-  createCanvas(1600,400);
-  speed=random(55,90);
-  weight=random(400,1500);
-  bullet= createSprite(50,200,50,50);
-  bullet.velocityX=speed;
-  wall = createSprite(1500,200,60, height/2);
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
   
-
-
 }
+
 
 function draw() {
-  background("black");
-  
-  if(wall.x-bullet.x<(bullet.width+wall.width)/2){
-
-   bullet.velocityX=0;
-   var deformation= 0.5 * weidht * speed * speed/22509;
-   if(deformation>180){
-     bullet.shapeColor=color(255,0,0);
-   } 
-
-   if(deformation<180 && deformation>180){
-    bullet.shapeColor=color(230,230,0);
-   }
-   
-   if(deformation<100){
-     bullet.shapecolor=color(0,255,0);
-   }
-  }
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
   drawSprites();
+ 
 }
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+    Matter.Body.setStatic(packageBody, false);
+  }
+}
+
+
 
